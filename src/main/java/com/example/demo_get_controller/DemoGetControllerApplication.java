174bb88b767b1cfe73e7b7fb574a6ffa61ed9f6a
Dennis Ciccone <dennisciccone02@gmail.com>
2025-07-03 15:13:01 +0200

    Initial commit: project setup

---
+++ src/main/java/com/example/demo_get_controller/DemoGetControllerApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_get_controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGetControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGetControllerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo_get_controller/DemoGetControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_get_controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGetControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
